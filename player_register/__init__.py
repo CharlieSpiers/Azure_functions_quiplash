13b0a92b6aa67fa4c1ce846b9204e198992c9ba6
CharlieSpiers <78343069+CharlieSpiers@users.noreply.github.com>
2022-11-07 11:59:20 +0000

    Started writing testing for player functions

--- player_register/__init__.py
+++ player_register/__init__.py
@@ -9,14 +9,14 @@ from database_functions import add_player, player_already_exists_exception
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    player_name = req.get_json().get('username')
-    player_password = req.get_json().get('password')
+    player_name = req.get_json().get("username")
+    player_password = req.get_json().get("password")
 
-    if not (4 < len(player_name) < 16):
+    if not (4 <= len(player_name) <= 16):
         bad_username_message = "Username less than 4 characters or more than 16 characters"
         return func.HttpResponse(body=json.dumps({"result": False, "msg": bad_username_message}))
 
-    elif not (8 < len(player_password) < 24):#
+    elif not (8 <= len(player_password) <= 24):
         bad_password_message = "Password less than 8 characters or more than 24 characters"
         return func.HttpResponse(body=json.dumps({"result": False, "msg": bad_password_message}))
 
